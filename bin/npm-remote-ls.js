a50bb1ea2a3f1714b80ed2c8157a369a10a0cb02
Forrest L Norvell <forrest@npmjs.com>
2015-12-03 19:30:10 -0800

    src: convert code to standard

--- bin/npm-remote-ls.js
+++ bin/npm-remote-ls.js
@@ -1,64 +1,64 @@
 #!/usr/bin/env node
 
 var yargs = require('yargs')
-  .options('n', {
-    alias: 'name',
-    description: 'package name'
-  })
-  .options('v', {
-    alias: 'version',
-    description: 'package version',
-    default: 'latest'
-  })
-  .options('e', {
-    alias: 'verbose',
-    description: 'enable verbose logging',
-    default: false,
-    boolean: true
-  })
-  .options('d', {
-    alias: 'development',
-    description: 'show development dependencies',
-    default: true,
-    boolean: true
-  })
-  .options('o', {
-    alias: 'optional',
-    description: 'show optional dependencies',
-    default: true,
-    boolean: true
-  })
-  .options('r', {
-    alias: 'registry',
-    description: 'set an alternative registry url',
-    default: require('registry-url')
-  })
-  .options('f', {
-    alias: 'flatten',
-    description: 'return flat representation of dependencies',
-    default: false,
-    boolean: true
-  }),
-  ls = require('../lib').ls,
-  treeify = require('treeify'),
-  spinner = require("char-spinner");
+    .options('n', {
+      alias: 'name',
+      description: 'package name'
+    })
+    .options('v', {
+      alias: 'version',
+      description: 'package version',
+      default: 'latest'
+    })
+    .options('e', {
+      alias: 'verbose',
+      description: 'enable verbose logging',
+      default: false,
+      boolean: true
+    })
+    .options('d', {
+      alias: 'development',
+      description: 'show development dependencies',
+      default: true,
+      boolean: true
+    })
+    .options('o', {
+      alias: 'optional',
+      description: 'show optional dependencies',
+      default: true,
+      boolean: true
+    })
+    .options('r', {
+      alias: 'registry',
+      description: 'set an alternative registry url',
+      default: require('registry-url')
+    })
+    .options('f', {
+      alias: 'flatten',
+      description: 'return flat representation of dependencies',
+      default: false,
+      boolean: true
+    })
+var ls = require('../lib').ls
+var treeify = require('treeify')
+var spinner = require('char-spinner')
 
 require('../lib').config({
   verbose: yargs.argv.verbose,
   development: yargs.argv.development,
   optional: yargs.argv.optional,
   registry: yargs.argv.registry
-});
+})
 
-var name = yargs.argv.name || yargs.argv._[0] || '',
-  version = name.split('@')[1] || yargs.argv.version;
+var name = yargs.argv.name || yargs.argv._[0] || ''
+var version = name.split('@')[1] || yargs.argv.version
 
 if (yargs.argv.help || !name) {
-  console.log(yargs.help());
+  console.log(yargs.help())
 } else {
-  spinner();
-  ls(name.split('@')[0], version, yargs.argv.flatten, function(obj) {
-    if (Array.isArray(obj)) console.log(obj);
-    else console.log(treeify.asTree(obj));
-  });
+  spinner()
+  ls(name.split('@')[0], version, yargs.argv.flatten, function (obj) {
+    if (Array.isArray(obj)) console.log(obj)
+    else console.log(treeify.asTree(obj))
+  })
 }
--- test/remote-ls-test.js
+++ test/remote-ls-test.js
@@ -1,189 +1,186 @@
-var Lab = require('lab'),
-  lab = exports.lab = Lab.script(),
-  nock = require('nock'),
-  fs = require('fs'),
-  RemoteLS = require('../lib/remote-ls');
+var Lab = require('lab')
+var lab = exports.lab = Lab.script()
+var nock = require('nock')
+var fs = require('fs')
+var RemoteLS = require('../lib/remote-ls')
 
-lab.experiment('RemoteLS', function() {
-
-  lab.experiment('guessVersion', function() {
-    lab.it('should handle an exact version being provided', function(done) {
-      var versionString = '1.0.0',
-        packageJson = JSON.parse(
-          fs.readFileSync('./test/fixtures/nopt.json').toString()
-        ),
-        ls = new RemoteLS();
-
-      Lab.expect(
-        ls._guessVersion(versionString, packageJson)
-      ).to.eql('1.0.0');
-
-      done();
-    });
-
-    lab.it('should handle a complex version being provided', function(done) {
-      var versionString = '*',
-        packageJson = JSON.parse(
-          fs.readFileSync('./test/fixtures/nopt.json').toString()
-        ),
-        ls = new RemoteLS();
-
-      Lab.expect(
-        ls._guessVersion(versionString, packageJson)
-      ).to.eql('3.0.1');
-
-      done();
-    });
-
-    lab.it('should raise an exception if version cannot be found', function(done) {
-      var versionString = '9.0.0',
-        packageJson = JSON.parse(
-          fs.readFileSync('./test/fixtures/nopt.json').toString()
-        ),
-        ls = new RemoteLS();
-
-      Lab.expect(function() {
-        ls._guessVersion(versionString, packageJson)
-      }).to.throw(/could not find a satisfactory version/);
-
-      done();
-    });
-
-    lab.it('should raise an exception if version cannot be found', function(done) {
-      var versionString = '9.0.0',
-        packageJson = JSON.parse(
-          fs.readFileSync('./test/fixtures/nopt.json').toString()
-        ),
-        ls = new RemoteLS();
-
-      Lab.expect(function() {
-        ls._guessVersion(versionString, packageJson)
-      }).to.throw(/could not find a satisfactory version/);
-
-      done();
-    });
-
-    lab.it('should handle "latest" being provided as version', function(done) {
-      var versionString = 'latest',
-        packageJson = JSON.parse(
-          fs.readFileSync('./test/fixtures/nopt.json').toString()
-        ),
-        ls = new RemoteLS();
-
-      Lab.expect(
-        ls._guessVersion(versionString, packageJson)
-      ).to.eql('3.0.1');
-
-      done();
-    });
-  });
-
-  lab.experiment('_walkDependencies', function() {
-    lab.it('should push appropriate dependencies to queue', function(done) {
+lab.experiment('RemoteLS', function () {
+  lab.experiment('guessVersion', function () {
+    lab.it('should handle an exact version being provided', function (done) {
+      var versionString = '1.0.0'
       var packageJson = JSON.parse(
-          fs.readFileSync('./test/fixtures/nopt.json').toString()
-        ),
-        ls = new RemoteLS({
-          queue: {
-            pause: function() {},
-            push: function(obj) {
-              Lab.expect(obj.name).to.eql('abbrev');
-              Lab.expect(obj.version).to.eql('1');
-              done();
-            }
+        fs.readFileSync('./test/fixtures/nopt.json').toString()
+      )
+      var ls = new RemoteLS()
+
+      Lab.expect(
+        ls._guessVersion(versionString, packageJson)
+      ).to.eql('1.0.0')
+
+      done()
+    })
+
+    lab.it('should handle a complex version being provided', function (done) {
+      var versionString = '*'
+      var packageJson = JSON.parse(
+        fs.readFileSync('./test/fixtures/nopt.json').toString()
+      )
+      var ls = new RemoteLS()
+
+      Lab.expect(
+        ls._guessVersion(versionString, packageJson)
+      ).to.eql('3.0.1')
+
+      done()
+    })
+
+    lab.it('should raise an exception if version cannot be found', function (done) {
+      var versionString = '9.0.0'
+      var packageJson = JSON.parse(
+        fs.readFileSync('./test/fixtures/nopt.json').toString()
+      )
+      var ls = new RemoteLS()
+
+      Lab.expect(function () {
+        ls._guessVersion(versionString, packageJson)
+      }).to.throw(/could not find a satisfactory version/)
+
+      done()
+    })
+
+    lab.it('should raise an exception if version cannot be found', function (done) {
+      var versionString = '9.0.0'
+      var packageJson = JSON.parse(
+        fs.readFileSync('./test/fixtures/nopt.json').toString()
+      )
+      var ls = new RemoteLS()
+
+      Lab.expect(function () {
+        ls._guessVersion(versionString, packageJson)
+      }).to.throw(/could not find a satisfactory version/)
+
+      done()
+    })
+
+    lab.it('should handle "latest" being provided as version', function (done) {
+      var versionString = 'latest'
+      var packageJson = JSON.parse(
+        fs.readFileSync('./test/fixtures/nopt.json').toString()
+      )
+      var ls = new RemoteLS()
+
+      Lab.expect(
+        ls._guessVersion(versionString, packageJson)
+      ).to.eql('3.0.1')
+
+      done()
+    })
+  })
+
+  lab.experiment('_walkDependencies', function () {
+    lab.it('should push appropriate dependencies to queue', function (done) {
+      var packageJson = JSON.parse(
+        fs.readFileSync('./test/fixtures/nopt.json').toString()
+      )
+      var ls = new RemoteLS({
+        queue: {
+          pause: function () {},
+          push: function (obj) {
+            Lab.expect(obj.name).to.eql('abbrev')
+            Lab.expect(obj.version).to.eql('1')
+            done()
           }
-        });
+        }
+      })
 
       ls._walkDependencies({
         name: 'nopt',
         version: '1.0.6',
         parent: {}
-      }, packageJson, function() {});
-    });
+      }, packageJson, function () {})
+    })
 
-    lab.it('should push devDependencies to queue', function(done) {
+    lab.it('should push devDependencies to queue', function (done) {
       var packageJson = JSON.parse(
-          fs.readFileSync('./test/fixtures/nopt.json').toString()
-        ),
-        ls = new RemoteLS({
-          queue: {
-            pause: function() {},
-            push: function(obj) {
-              Lab.expect(obj.name).to.eql('tap');
-              Lab.expect(obj.version).to.eql('1.0.0');
-              done();
-            }
+        fs.readFileSync('./test/fixtures/nopt.json').toString()
+      )
+      var ls = new RemoteLS({
+        queue: {
+          pause: function () {},
+          push: function (obj) {
+            Lab.expect(obj.name).to.eql('tap')
+            Lab.expect(obj.version).to.eql('1.0.0')
+            done()
           }
-        });
+        }
+      })
 
       ls._walkDependencies({
         name: 'nopt',
         version: '1.0.8',
         parent: ls.tree
-      }, packageJson, function() {});
-    });
+      }, packageJson, function () {})
+    })
 
-    lab.it('should not raise an exception if package has no dependencies', function(done) {
+    lab.it('should not raise an exception if package has no dependencies', function (done) {
       var packageJson = JSON.parse(
-          fs.readFileSync('./test/fixtures/abbrev.json').toString()
-        ),
-        ls = new RemoteLS();
+        fs.readFileSync('./test/fixtures/abbrev.json').toString()
+      )
+      var ls = new RemoteLS()
 
       ls._walkDependencies({
         name: 'abbrev',
         version: '*',
         parent: {}
-      }, packageJson, function() {});
+      }, packageJson, function () {})
 
-      done();
-    });
+      done()
+    })
 
-    lab.it("should not walk dependency if dependency has already been observed", function(done) {
+    lab.it('should not walk dependency if dependency has already been observed', function (done) {
       var packageJson = JSON.parse(
-          fs.readFileSync('./test/fixtures/nopt.json').toString()
-        ),
-        ls = new RemoteLS({
-          flat: {
-            'nopt@1.0.0': true
-          },
-          queue: {
-            pause: function() {},
-            push: function(obj) {
-              Lab.expect(obj.name).to.eql('tap');
-              Lab.expect(obj.version).to.eql('1.0.0');
-              done();
-            }
+        fs.readFileSync('./test/fixtures/nopt.json').toString()
+      )
+      var ls = new RemoteLS({
+        flat: {
+          'nopt@1.0.0': true
+        },
+        queue: {
+          pause: function () {},
+          push: function (obj) {
+            Lab.expect(obj.name).to.eql('tap')
+            Lab.expect(obj.version).to.eql('1.0.0')
+            done()
           }
-        });
+        }
+      })
 
       ls._walkDependencies({
         name: 'nopt',
         version: '1.0.0',
         parent: {}
-      }, packageJson, function() {});
+      }, packageJson, function () {})
 
-      done();
-    });
-  });
+      done()
+    })
+  })
 
-  lab.experiment('ls', function() {
-    lab.it('handles a 404 and prints an appropriate message', function(done) {
-      var missingPackage = nock('https://skimdb.npmjs.com')
-        .get('/registry/request')
-        .reply(404),
-        ls = new RemoteLS({
-          registry: 'https://skimdb.npmjs.com/registry/',
-          logger: {
-            log: function(msg) {
-              Lab.expect(msg).to.match(/status = 404/);
-              done();
-            }
+  lab.experiment('ls', function () {
+    lab.it('handles a 404 and prints an appropriate message', function (done) {
+      nock('https://skimdb.npmjs.com')
+          .get('/registry/request')
+          .reply(404)
+      var ls = new RemoteLS({
+        registry: 'https://skimdb.npmjs.com/registry/',
+        logger: {
+          log: function (msg) {
+            Lab.expect(msg).to.match(/status = 404/)
+            done()
           }
-        });
+        }
+      })
 
-      ls.ls('request', '*', function() {});
-    });
-
-  });
-
-});
+      ls.ls('request', '*', function () {})
+    })
+  })
+})
